327afd9bee9536e9b374a91d13415bfd74448d10
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2020-11-19 15:16:33 +0900

    TestCode CopyCoding중..

--- src/main/java/com/example/demo/Repository/OwnerRepository.java
+++ src/main/java/com/example/demo/Repository/OwnerRepository.java
@@ -3,7 +3,9 @@ package com.example.demo.Repository;
 
 import com.example.demo.model.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OwnerRepository extends JpaRepository<Owner, Integer> {
 
 }
---
+++ src/main/java/com/example/demo/Repository/VetRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.Repository;
+
+import com.example.demo.model.Vet;
+import org.springframework.stereotype.Repository;
+
+public interface VetRepository extends Repository<Vet,Integer> {
+}
